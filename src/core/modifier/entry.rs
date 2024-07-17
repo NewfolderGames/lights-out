96d7b482f3c84acd74cde7dd18719a85435dc88d
이민용 <newtolder@koolsign.net>
2024-07-17 11:50:46 +0900

    Reformat files

--- src/core/modifier/entry.rs
+++ src/core/modifier/entry.rs
@@ -6,54 +6,54 @@ pub struct ModifierEntry {
 }
 
 impl ModifierEntry {
-    
+
     pub fn new(name: String, value: f64, count: i32, calculation: ModifierCalculationMethod) -> Self {
-        
+
         Self {
             name,
             value,
             count,
             calculation
         }
-        
+
     }
-    
+
     pub fn get_name(&self) -> &str {
-        
+
         self.name.as_str()
-        
+
     }
-    
+
     pub fn get_value(&self) -> f64 {
-        
+
         self.value
-        
+
     }
-    
+
     pub fn get_calculation_method(&self) -> ModifierCalculationMethod {
-        
+
         self.calculation
-        
+
     }
-    
+
     pub fn get_count(&self) -> i32 {
-        
+
         self.count
-        
+
     }
-    
+
     pub fn set_count(&mut self, count: i32) {
-        
+
         self.count = count;
-        
+
     }
-    
+
     pub fn add_count(&mut self, count: i32) {
-        
+
         self.count += count;
-        
+
     }
-    
+
 }
 
 #[derive(Copy, Clone)]
